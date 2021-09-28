e1b53ce8c43fa63a5bd5521c1db2d74d216f142c
takusan64 <hafe1119@yahoo.co.jp>
2021-09-28 20:39:43 +0900

    move openapi.json.

--- backend/openapi/openapi_util.py
+++ backend/openapi/openapi_util.py
@@ -1,10 +1,9 @@
 import json
-import config
 
 def create_openapi_jsonfile(app):
   try:
     openapi_data = app.openapi()
-    with open('openapi.json', 'w') as f:
+    with open('./openapi/openapi.json', 'w') as f:
       json.dump(openapi_data, f, ensure_ascii=False, indent=2)
     print("Created OpenAPI JSON File")
   except Exception:
